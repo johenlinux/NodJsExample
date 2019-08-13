0efc390233ca65a27cdc617d48d559adc8be8994
johnelinux <johenlinuxthettun@gmail.com>
2019-08-13 15:30:47 +0630

    Add model

--- controllers/product.js
+++ controllers/product.js
@@ -1,15 +1,17 @@
-const porducts = [];
+const Product = require('../models/product.js')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin', { pageTitle: 'Admin' });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    porducts.push({title: req.body.title});
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProduct = (req, res, next) => {
+    const porducts = Product.fetchAll();
     res.render('shop', {
         prod: porducts,
         pageTitle: 'shop',
